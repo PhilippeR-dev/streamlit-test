0c79722b5090e4fbd030f22a47ffefda6b66039a
Philippe REY <65810465+PhilippeR-dev@users.noreply.github.com>
2023-08-02 13:38:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,24 @@
 import streamlit as st
 # import graphviz
+""
+  # Create a graphlib graph object
+  graph = graphviz.Digraph()
+  graph.edge('run', 'intr')
+  graph.edge('intr', 'runbl')
+  graph.edge('runbl', 'run')
+  graph.edge('run', 'kernel')
+  graph.edge('kernel', 'zombie')
+  graph.edge('kernel', 'sleep')
+  graph.edge('kernel', 'runmem')
+  graph.edge('sleep', 'swap')
+  graph.edge('swap', 'runswap')
+  graph.edge('runswap', 'new')
+  graph.edge('runswap', 'runmem')
+  graph.edge('new', 'runmem')
+  graph.edge('sleep', 'runmem')
+""
 
-# Create a graphlib graph object
-graph = graphviz.Digraph()
-graph.edge('run', 'intr')
-graph.edge('intr', 'runbl')
-graph.edge('runbl', 'run')
-graph.edge('run', 'kernel')
-graph.edge('kernel', 'zombie')
-graph.edge('kernel', 'sleep')
-graph.edge('kernel', 'runmem')
-graph.edge('sleep', 'swap')
-graph.edge('swap', 'runswap')
-graph.edge('runswap', 'new')
-graph.edge('runswap', 'runmem')
-graph.edge('new', 'runmem')
-graph.edge('sleep', 'runmem')
+# st.graphviz_chart(graph)
 
-st.graphviz_chart(graph)
+number = st.slider("Pick a number: ", min_value=1, max_value=10)
+st.text("Your number is " + str(number))
