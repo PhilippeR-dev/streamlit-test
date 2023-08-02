79d93a04d188386f14be668cb4c5ee5c92c7d1f4
Philippe REY <65810465+PhilippeR-dev@users.noreply.github.com>
2023-08-02 12:50:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,9 @@
+import streamlit as st
+import graphviz
 
+st.graphviz_chart('''digraph {
+    a [shape="ellipse" style="filled" fillcolor="#1f77b4"]
+    b [shape="polygon" style="filled" fillcolor="#ff7f0e"]
+    a -> b [fillcolor="#a6cee3" color="#1f78b4"]
+}
+''')
